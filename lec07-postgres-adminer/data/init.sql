374b93f85edeb11cd4967070af78e0d8c2d430dd
anuj <ganuj0302@gmail.com>
2024-12-19 12:41:31 +0530

    code updated

---
+++ lec07-postgres-adminer/data/init.sql
@@ -0,0 +1,12 @@
+create table product(
+  id serial PRIMARY KEY,
+  name VARCHAR(50),
+  price numeric(10,2) NOT NULL
+);
+
+
+insert into product(name, price)
+values 
+('tv', 100.12),
+('iphone', 600.12),
+('ipad', 50.12)
